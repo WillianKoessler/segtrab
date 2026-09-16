5ead50ce2a60b4eb4fc224d62968fe4a34e8f1f2
WillianKoessler <43802120+WillianKoessler@users.noreply.github.com>
2026-09-16 12:11:50 -0300

    [FE] added sleep function for debugging in utilities

--- Laravel/resources/js/lib/utils.js
+++ Laravel/resources/js/lib/utils.js
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export async function sleep(ms) {
+    await (new Promise(resolve => setTimeout(resolve, Math.random() * 2000)));
+}
